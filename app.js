491935336b33b5994258f8f0e155223a35ae808d
King.Zevin <king.zevin@gmail.com>
2020-09-30 09:54:22 +0800

    v2.0 - unify the migration

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-
+process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`;
 const logger = require('logger-sharelatex')
 const settings = require('settings-sharelatex')
 
@@ -37,32 +37,33 @@ function __guard__(value, transform) {
     : undefined
 }
 
-exports.main = test
-function test(params = {}) {
-  // params e.g.: {
-  //  url: '/user/5e9723ee71ffbe00909ed452/contacts',
-  //  method: 'get',
-  // }
+exports.main = main
+function main(params = {}){
+  const url = params.__ow_path
+  const method = params.__ow_method
+  const headers = params.__ow_headers
   
-  const url = params.__ow_path || '/project/5ee9ea6bd9085c0007b38bfe/messages';
-  const method = params.__ow_method || 'get';
-
   const { promisify } = require('util')
   const request = require("request")
   const reqPromise = promisify(request[method]);
-
   return (async () => {
-    const result = await reqPromise({
-      url: `http://${host}:${port}${url}`,
-      json: params
-    })
-    return result
-  })();
-}
-
-if (!module.parent) {
-  (async ()=>{
-    let a = await test();
-    console.log(a);
+    let result;
+    let opt={}
+    opt['headers'] = headers;
+    opt['url'] = `http://${host}:${port}${url}`;
+    let str = params.__ow_body || '';
+    if(str !== "" && Buffer.from(str, 'base64').toString('base64') === str){
+      // base64
+      params.__ow_body = Buffer.from(str, 'base64').toString('ascii');
+    }
+    opt['body'] = params.__ow_body;
+    if(params.__ow_query !== ""){
+      const qs = '?' + params.__ow_query;
+      opt['url'] = opt['url'] + qs;
+    }
+    result = await reqPromise(opt);
+    var response = JSON.parse(JSON.stringify(result));
+    delete response.request
+    return response
   })();
 }
--- config/settings.defaults.js
+++ config/settings.defaults.js
@@ -6,9 +6,27 @@ module.exports = {
     }
   },
 
-  mongo: {
-    url:
-      `mongodb://172.17.0.1:27017/sharelatex`
+  apis: {
+    web: {
+      url: `http://${process.env['WEB_HOST'] || 'localhost'}:${process.env[
+        'WEB_PORT'
+      ] || 3000}`,
+      user: process.env['WEB_API_USER'] || 'sharelatex',
+      pass: process.env['WEB_API_PASSWORD'] || 'password'
+    }
   },
 
+  mongo: {
+    url:
+      process.env['MONGO_CONNECTION_STRING'] ||
+      `mongodb://${process.env['MONGO_HOST'] || 'localhost'}/sharelatex`
+  },
+
+  redis: {
+    web: {
+      host: process.env['REDIS_HOST'] || 'localhost',
+      port: '6379',
+      password: process.env['REDIS_PASSWORD'] || ''
+    }
+  }
 }
