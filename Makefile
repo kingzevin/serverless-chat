a076465ae1ead9525ae6004083261720502e91f6
King.Zevin <king.zevin@gmail.com>
2020-10-23 10:41:38 +0800

    v3.1 - updated the index and Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ all: update
 
 update: 
 	./rezip.sh chat
-	wsk -i action update /guest/sharelatex/chat chat.zip --kind  nodejs:10 --web true
+	wsk -i action update /guest/sharelatex/chat chat.zip --kind  nodejs:10 --web raw
 
 
--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict'
 // environment variables specified by the user
-process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // chat.config.env
+process.env["MONGO_CONNECTION_STRING"] = `mongodb://172.17.0.1:27017/sharelatex`; // chat.config.envs
 // the user should specify the express listener
-const expressListener = require('./app.js') // chat.express.file
+const expressListener = require('./app.js') // chat.express.object
 
 const owServerlessExpress = require('./owServerlessExpress.js')
 
-exports.main = function(params){ // chat.handler.function
+exports.main = function(params){ 
   return owServerlessExpress(expressListener, params)
 }
